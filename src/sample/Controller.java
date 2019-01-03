8cfd84fe70adce0a481d6257a75cf70872c19029
BT_1N3_30 <cyang4980@bths.edu>
2019-01-03 09:53:51 -0500

    Visual Update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,48 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
+import java.util.ArrayList;
+
 public class Controller {
+
+    int patternNum = 0;
+
+    ArrayList<String> pattern = new ArrayList<String>();
+    boolean playerTurn = false;
+
+    public void generateSaid() {
+        patternNum++;
+        for (int i = 0; i < patternNum; i++) {
+            int random = (int) (Math.random() * 4);
+            if (random == 1) {
+                pattern.add("Fire");
+            }
+            if (random == 2) {
+                pattern.add("Water");
+            }
+            if (random == 3) {
+                pattern.add("Air");
+            }
+            if (random == 4) {
+                pattern.add("Earth");
+            }
+        }
+    }
+
+    public void addFire(ActionEvent event) {
+        System.out.println("Fire");
+    }
+
+    public void addWater(ActionEvent event) {
+        System.out.println("Water");
+    }
+
+    public void addAir(ActionEvent event) {
+        System.out.println("Air");
+    }
+
+    public void addEarth(ActionEvent event) {
+        System.out.println("Earth");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
+        Image image = new Image("url:http://blog.world-mysteries.com/wp-content/uploads/2018/01/hieroglyphsEG.jpg");
+        ImageView mv = new ImageView(image);
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 450, 450));
+        primaryStage.setScene(new Scene(root, 700, 595));
         primaryStage.show();
     }
 
